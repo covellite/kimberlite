a93a0d9d40237391bd5107fc60d542e9b50d24a1
covellite <covellite.jp@gmail.com>
2014-06-16 12:27:58 +0900

    140616 調整

--- script/kimberlite.js
+++ script/kimberlite.js
@@ -1,43 +1,45 @@
-(function ($) {
+;(function ($) {
     'use strict';
 
     $.fn.kimberlite = function (o) {
         var d = {
-            scrollPoint: 200,
-            scrollSpeed: 600,
+            displayPoint: 200,
+            scrollSpeed: 1000,
             displaySpeed: 100,
             showBottom: "20px",
-            hideBottom: "-200px"
+            hideBottom: "-200px",
+            flag: false
         },
             fo = $.extend({}, d, o);
 
         return this.each(function () {
             var pagetop = $(this);
-            $(window).scroll(function(e) {
-                if ($(this).scrollTop() >= fo.scrollPoint) {
-                    pagetop
-                        .animate({
-                            bottom: fo.showBottom
-                        },fo.displaySpeed);
+            $(window).scroll(function () {
+                if ($(this).scrollTop() > fo.displayPoint) {
+                    if (!fo.flag) {
+                        fo.flag = true;
+                        pagetop
+                            .animate({
+                                bottom: fo.showBottom
+                            }, fo.displaySpeed);
+                    }
                 } else {
-                    pagetop
-                        .animate({
-                            bottom: fo.hideBottom
-                        },fo.displaySpeed);
+                    if (fo.flag) {
+                        fo.flag = false;
+                        pagetop
+                            .animate({
+                                bottom: fo.hideBottom
+                            }, fo.displaySpeed);
+                    }
                 }
             });
 
             pagetop
                 .click(function (e) {
-                    $('body')
+                    $('html,body')
                         .animate({
                             scrollTop: 0
-                        }, fo.scrollSpeed, function() {
-                            $(this)
-                                .animate({
-                                    bottom: fo.hideBottom
-                                },0);
-                        });
+                        }, fo.scrollSpeed);
                     e.preventDefault();
                 });
         });
